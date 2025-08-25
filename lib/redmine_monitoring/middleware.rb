8214af4acf134377ae219f76fbb19dad2e78782f
Ruslan Fedotov <progruson@gmail.com>
2025-08-25 19:35:22 +0700

    Fix Middleware

--- lib/redmine_monitoring/middleware.rb
+++ lib/redmine_monitoring/middleware.rb
@@ -5,44 +5,48 @@ module RedmineMonitoring
     end
 
     def call(env)
-      begin
-        status, headers, response = @app.call(env)
-        log_http_error(status, env) if status >= 400
-        [status, headers, response]
-      rescue => e
-        log_exception(e, env)
-        save_error_to_db(e, env)
+      status, headers, response = @app.call(env)
+
+      if status >= 400
+        log_http_error(status, env)
+        save_error_to_db(status: status, env: env)
       end
+
+      [status, headers, response]
+    rescue => e
+      log_exception(e, env)
+      save_error_to_db(exception: e, env: env)
     end
 
     private
 
     def log_exception(exception, env)
-      Rails.logger.error "[Monitoring] #{exception.class}: #{exception.message}"
-      Rails.logger.error exception.backtrace.join("\n")
-      Rails.logger.error "URL: #{env['REQUEST_METHOD']} #{env['PATH_INFO']}"
+      logger = monitoring_logger
+      logger.error "[Monitoring] #{exception.class}: #{exception.message}"
+      logger.error exception.backtrace.join("\n")
+      logger.error "URL: #{env['REQUEST_METHOD']} #{env['PATH_INFO']}"
     end
 
     def log_http_error(status, env)
-      Logger.new("log/monitoring.log")
-      Rails.logger.warn "[Monitoring] HTTP #{status} on #{env['REQUEST_METHOD']} #{env['PATH_INFO']}"
+      logger = monitoring_logger
+      logger.warn "[Monitoring] HTTP #{status} on #{env['REQUEST_METHOD']} #{env['PATH_INFO']}"
     end
 
-    def save_error_to_db(exception, env)
+    def save_error_to_db(exception: nil, status: nil, env: )
       request = ActionDispatch::Request.new(env)
       controller_instance = env['action_controller.instance']
 
-      MonitoringError.create(
-        exception_class: exception.class.to_s,
-        error_class: exception.class.to_s,
-        message: exception.message.to_s,
-        backtrace: Array(exception.backtrace).join("\n"),
-        status_code: 500,
+      error_params = {
+        exception_class: exception ? exception.class.to_s : "HTTPError",
+        error_class: exception ? exception.class.to_s : "HTTP #{status}",
+        message: exception ? exception.message.to_s : ([status, MonitoringError::HTTP_STATUS_TEXT[status]].join(" ") rescue "Error #{status}"),
+        backtrace: exception ? Array(exception.backtrace).join("\n") : "",
+        status_code: exception ? 500 : status,
         controller_name: controller_instance&.class&.name || request.params['controller'],
         action_name: controller_instance&.action_name || request.params['action'],
         format: safe_format(request),
-        file: backtrace_file_line(exception).first,
-        line: backtrace_file_line(exception).last,
+        file: exception ? backtrace_file_line(exception).first : "",
+        line: exception ? backtrace_file_line(exception).last : "",
         user_id: (User.current&.id rescue nil),
         ip_address: request.remote_ip,
         user_agent: request.user_agent,
@@ -51,7 +55,12 @@ module RedmineMonitoring
         headers: filtered_headers(request).to_json,
         env: Rails.env,
         severity: 'fatal'
-      )
+      }
+
+      MonitoringError.create!(error_params)
+    rescue => e
+      logger = monitoring_logger
+      logger.error "[Monitoring] Ошибка при сохранении HTTP #{status}: #{e.class} #{e.message}"
     end
 
     def safe_params(params)
@@ -80,6 +89,10 @@ module RedmineMonitoring
       else
         [nil, nil]
       end
+    end
+
+    def monitoring_logger
+      Logger.new("log/monitoring.log")
     end
   end
 end
