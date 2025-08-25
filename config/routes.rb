ea3770eebc72bbe2a863de2abaf1cd7a84849dfe
Ruslan Fedotov <progruson@gmail.com>
2025-08-25 17:24:24 +0700

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
+
+get 'monitoring', to: 'monitoring_errors#index', as: 'monitoring_errors'
+get 'monitoring/test_error', to: 'monitoring_errors#test_error', as: 'monitoring_test_error'
+post 'monitoring/clear', to: 'monitoring_errors#clear', as: 'monitoring_clear'
