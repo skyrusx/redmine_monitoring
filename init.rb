354ba45a6ef162c81057dbe316ed946e35b6e758
Ruslan Fedotov <progruson@gmail.com>
2025-09-01 15:05:10 +0700

    Fix version 0.0.9 -> 0.0.10

--- init.rb
+++ init.rb
@@ -9,7 +9,7 @@ Redmine::Plugin.register :redmine_monitoring do
   name 'Redmine Monitoring plugin'
   author 'Ruslan Fedotov'
   description 'Error & performance monitoring'
-  version '0.0.9'
+  version '0.0.10'
   url 'https://github.com/skyrusx/redmine_monitoring'
   author_url 'https://github.com/skyrusx/'
 
